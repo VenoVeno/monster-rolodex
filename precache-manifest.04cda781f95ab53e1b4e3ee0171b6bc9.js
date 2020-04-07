fb4e07a7b369b3326eb0e9579d19af978d5dc4dd
Veno <you@example.com>
2020-04-07 18:36:05 +0530

    Updates

---
+++ precache-manifest.04cda781f95ab53e1b4e3ee0171b6bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b82629b5ee82996729e459568bffc95",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "545e849257fbc26743cf",
+    "url": "/monster-rolodex/static/css/main.a7a5e2a9.chunk.css"
+  },
+  {
+    "revision": "71230c7405385ba52369",
+    "url": "/monster-rolodex/static/js/2.071e6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.071e6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545e849257fbc26743cf",
+    "url": "/monster-rolodex/static/js/main.ca6907ec.chunk.js"
+  },
+  {
+    "revision": "c50c24058a8f35eccc4e",
+    "url": "/monster-rolodex/static/js/runtime-main.54f713db.js"
+  }
+]);
